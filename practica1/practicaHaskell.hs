09c0ab3f4fa0e7391bbb7482da5f208f74bfed3e
Adrián Egea <adrianensis@gmail.com>
2016-10-27 10:55:56 +0200

    ejercicio 6

--- practica1/practicaHaskell.hs
+++ practica1/practicaHaskell.hs
@@ -1,14 +1,32 @@
-cuadrado:: Float -> Float
+cuadrado:: Integer -> Integer
 cuadrado x = x*x
 
-cuarta:: Float -> Float
+cuarta:: Integer -> Integer
 cuarta x = cuadrado(cuadrado x)
 
-maximo:: Float -> Float -> Float
+maximo:: Integer -> Integer -> Integer
 maximo x y = if x >= y then x else y
 
-area:: Float -> Float
+area:: Integer -> Integer
 area r = (22/7)*cuadrado(r)
 
-fib :: Float -> Float
+fib:: Integer -> Integer
 fib n = if n <= 1 then n else fib(n-1)+fib(n-2)
+
+absol:: Integer -> Integer
+absol x = if x < 0 then -x else x
+
+aumentar:: Integer −>  Integer
+aumentar x = cuadrado(x+1)
+
+aumentar2:: Integer −>  Integer
+aumentar2 x = cuadrado(x+1)
+
+sucesor :: Integer -> Integer
+sucesor = (+1)
+
+aumentar3 :: Integer -> Integer
+aumentar3 = cuadrado.sucesor
+
+aumentar4 :: Integer -> Integer
+aumentar4 = sucesor.cuadrado
